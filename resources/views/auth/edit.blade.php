94aa098c9d1bc1eeb176458582e5f307b1b90d33
Burxonov Alimardon <burxonov.ok@gmail.com>
2024-11-01 10:35:13 +0500

    edit to'g'rlandi

--- resources/views/auth/edit.blade.php
+++ resources/views/auth/edit.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Profile Edit')
 
 @section('content')
-    <!-- Main content -->
+    
     <main class="flex-grow container mx-auto px-4 py-8">
         <div class="max-w-2xl mx-auto bg-white p-8 rounded-lg shadow-md">
             <h1 class="text-2xl font-bold mb-6">Edit Profile</h1>
