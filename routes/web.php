bcfcd9d1b308c313618310a8aec03cc73758ed70
Alimardon Burxonov <burxonovok.@gmail.com>
2024-11-08 16:25:15 +0500

    web to'g'rlandi

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', [AuthController::class,'index'])->name('home'); 
 
 Route::get('/register', [AuthController::class,'registerForm'])->name('registerForm');
